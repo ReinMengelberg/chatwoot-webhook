02359a947121591baacbec3a962fdc2fd2ffb2c4
Reinhard Mengelberg <reinhard@mengelmann.nl>
2024-06-24 14:01:05 +0200

    Update webhook-endpoint.ts

--- src/backend/webhook-endpoint.ts
+++ src/backend/webhook-endpoint.ts
@@ -1,35 +1,40 @@
 import * as sdk from 'botpress/sdk'
 import { handleIncomingMessage } from './message-handler'
 
+const ALLOWED_HOST = '159.223.223.7' // Host allowed to send requests
+
 export const setupWebhookEndpoint = (bp: typeof sdk) => {
   const router = bp.http.createRouterForBot('botpress-rocketchat-webhook', { checkAuthentication: false })
 
   router.post('/webhook', async (ctx) => {
-    try {
-      const { messages, agent, visitor } = ctx.request.body
+    const hostHeader = ctx.headers['Host']
 
-      if (!messages || !agent || !visitor) {
-        ctx.status = 400
-        ctx.body = 'messages, agent, and visitor are required'
-        return
-      }
-
-      if (!agent.username.startsWith('aiex')) {
-        ctx.status = 200
-        ctx.body = 'Agent username does not match criteria, ignoring message'
-        return
-      }
-
-      await handleIncomingMessage(bp, ctx.request.body)
-      ctx.status = 200
-      ctx.body = 'Payload processed'
-    } catch (error) {
-      bp.logger.error('Error handling webhook', error)
-      ctx.status = 500
-      ctx.body = 'Internal Server Error'
+    if (!hostHeader || !hostHeader.includes(ALLOWED_HOST)) {
+      ctx.status = 403
+      ctx.body = 'Forbidden: Invalid Host'
+      return
     }
+
+    const { messages, agent, visitor } = ctx.request.body
+
+    if (!messages || !agent || !visitor) {
+      ctx.status = 400
+      ctx.body = 'messages, agent, and visitor are required'
+      return
+    }
+
+    if (!agent.username.startsWith('aiex')) {
+      ctx.status = 200
+      ctx.body = 'Agent username does not match criteria, ignoring message'
+      return
+    }
+
+    await handleIncomingMessage(bp, ctx.request.body)
+    ctx.status = 200
+    ctx.body = 'Payload processed'
   })
 }
+
 
 
 
