a04b65a3464a55f777b7c0a18aa95387abb36c06
Reinhard Mengelberg <reinhard@mengelmann.nl>
2024-06-28 21:08:27 +0200

    Update and rename api-endpoint.ts to template-endpoint.ts

--- src/backend/template-endpoint.ts
+++ src/backend/template-endpoint.ts
@@ -4,13 +4,13 @@ export default async (bp: typeof sdk) => {
 
   const JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImluZm9AbWVuZ2VsbWFubi5ubCIsInN0cmF0ZWd5IjoiZGVmYXVsdCIsInRva2VuVmVyc2lvbiI6MSwiaXNTdXBlckFkbWluIjp0cnVlLCJpYXQiOjE3MTg5ODY2ODUsImV4cCI6MjAzNDU2MjY4NSwiYXVkIjoiY29sbGFib3JhdG9ycyJ9.BY7suW6VDoJXDR7rsqMGoTlWUsgVAcKqQP4LXrVC7IQ"
 
-  export const setupAPIEndpoint = (bp: typeof sdk) => {
+  export const setupTemplateEndpoint = (bp: typeof sdk) => {
     const router = bp.http.createRouterForBot('rocketchat-webhook', { checkAuthentication: false })
   
     router.post('/api-endpoint', async (req, res) => {
       const tokenHeader = req.headers['Authorization']
   
-      if (!tokenHeader || tokenHeader !== JWT_TOKEN) {
+      if (!tokenHeader || tokenHeader !== `Bearer ${JWT_TOKEN}`) {
         res.status(403).send('Forbidden: Invalid Token')
         return
       }
