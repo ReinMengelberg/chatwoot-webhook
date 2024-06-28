c7096c5d80da5f9d48e5282d2e4a8114ce768765
Reinhard Mengelberg <reinhard@mengelmann.nl>
2024-06-28 21:11:03 +0200

    Update template-endpoint.ts

--- src/backend/template-endpoint.ts
+++ src/backend/template-endpoint.ts
@@ -1,26 +1,34 @@
 import * as sdk from 'botpress/sdk'
+import { handleOutgoingTemplate } from './template-handler'
 
-export default async (bp: typeof sdk) => {
+const ROCKETCHAT_TOKEN = "U8rL3F9nqzW1YbD7xM2aNvK6eX0pJcQ4sT5hZjVwP8mL1yRq"
 
-  const JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImluZm9AbWVuZ2VsbWFubi5ubCIsInN0cmF0ZWd5IjoiZGVmYXVsdCIsInRva2VuVmVyc2lvbiI6MSwiaXNTdXBlckFkbWluIjp0cnVlLCJpYXQiOjE3MTg5ODY2ODUsImV4cCI6MjAzNDU2MjY4NSwiYXVkIjoiY29sbGFib3JhdG9ycyJ9.BY7suW6VDoJXDR7rsqMGoTlWUsgVAcKqQP4LXrVC7IQ"
+export const setupWebhookEndpoint = (bp: typeof sdk) => {
+  const router = bp.http.createRouterForBot('rocketchat-webhook', { checkAuthentication: false })
 
-  export const setupTemplateEndpoint = (bp: typeof sdk) => {
-    const router = bp.http.createRouterForBot('rocketchat-webhook', { checkAuthentication: false })
-  
-    router.post('/api-endpoint', async (req, res) => {
-      const tokenHeader = req.headers['Authorization']
-  
-      if (!tokenHeader || tokenHeader !== `Bearer ${JWT_TOKEN}`) {
-        res.status(403).send('Forbidden: Invalid Token')
-        return
-      }
+  router.post('/template-endpoint', async (req, res) => {
+    const tokenHeader = req.headers['x-rocketchat-livechat-token']
 
-  // Link to access this route: http://localhost:3000/api/v1/bots/BOT_NAME/mod/starter-module/my-first-route
-  router.get('/my-first-route', async (req, res) => {
-    // Since the bot ID is required to access your module,
-    const botId = req.params.botId
+    if (!tokenHeader || tokenHeader !== ROCKETCHAT_TOKEN) {
+      res.status(403).send('Forbidden: Invalid Token')
+      return
+    }
 
+    // CHECK EVENT 
+    
+    const { messages, agent, visitor } = req.body
 
-    res.sendStatus(200)
+    if (!messages || !agent || !visitor) {
+      res.status(400).send('messages, agent, and visitor are required')
+      return
+    }
+
+    if (!agent.username.startsWith('aiex')) {
+      res.status(200).send('Agent username does not match criteria, ignoring message')
+      return
+    }
+
+    await handleIncomingMessage(bp, req.body)
+    res.status(200).send('Payload processed')
   })
 }
