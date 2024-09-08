49cffd6b60a18746b704c743a2a0a5b98314c444
Reinhard Mengelberg <reinhard@mengelmann.nl>
2024-09-08 17:19:12 +0200

    Update sfm-endpoint.ts

--- src/backend/sfm-endpoint.ts
+++ src/backend/sfm-endpoint.ts
@@ -14,17 +14,17 @@ export const setupSfmEndpoint = async (bp: typeof sdk) => {
     const {account, inbox, bot, contact, conversation, message} = req.body
 
     if (!account || !inbox || !bot || !contact || !conversation || !message) {
-      bp.logger.error('Invalid request payload: account, inbox, bot, contact, conversation and message are required')
-      res.status(400).send('Invalid request payload: account, inbox, bot, contact, conversation and message are required')
+      bp.logger.warning('Chatwoot-Webhook invalid request payload on /sfm-endpoint: account, inbox, bot, contact, conversation and message are required')
+      res.status(400).send('{"error":"account, inbox, bot, contact, conversation and messages are required"}')
       return
     }
 
     try {
       await sendFirstMessage(bp, req.body)
-      res.status(200).send('Payload processed')
+      res.status(200).send({"success":"payload processed"})
     } catch (error) {
-      bp.logger.error(`Error when sending first message: ${error}`)
-      res.status(500).send(`Error when sending first message: ${error}`)
+      bp.logger.error(`Error when sending first message recieved from /sfm-endpoint: ${error}`)
+      res.status(500).send(`{"error":"${error}"}`)
     }
   })
 }
